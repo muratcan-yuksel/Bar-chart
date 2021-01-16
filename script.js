c48377ffbeeb78d1719c750d1c24959e5e2c7676
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-16 23:16:15 +0300

    fetched the data into the html element <p>, for some unknown reason

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-/*
+
 
 let requestUrl= "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
 //Both of the following methods get the data correctly
 
 fetch(requestUrl)
 	.then(response => response.json())
 	.then(data => {
-console.log (JSON.stringify(data));
+    document.getElementById('message').innerHTML = JSON.stringify(data);
 	})
 
 
-*/
+
 /*
 const req = new XMLHttpRequest();
 req.open("GET",requestUrl, true);
@@ -22,6 +23,7 @@ console.log(JSON.stringify(json));
 */
 //this one get's the data in a different manner
 
+/*
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
 d3.autoType)
 .then (function(data){
@@ -34,3 +36,24 @@ d3.autoType)
   .style("width", function(d) { return d.amount * 40 + "px"; })
   .style("height", "15px");
 });
+*/
+/*
+var title = "Comments on Posts",
+  data = [{type: "Post A", amount: 4},
+         {type: "Post B", amount: 2},
+         {type: "Post C", amount: 7},
+         {type: "Post D", amount: 5},
+         {type: "Post E", amount: 6}]
+;
+
+d3.select("body")
+  .append("h3")
+  .text(title);
+
+  d3.select("body")
+  .selectAll("div")
+  .data(data)
+  .enter()
+  .append("div")
+  .style("width", function(d) { return d.amount * 40 + "px"; })
+  .style("height", "15px");*/
