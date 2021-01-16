ed7a3e6cb52fecfd5f36262a6ae64c324e4b7bcd
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-16 22:56:32 +0300

    added another data-get function, all 3 of them are working, this one works differently tho. Also added style css file, will continue modifying almost everything

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/*
+
 let requestUrl= "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 //Both of the following methods get the data correctly
-/*
+
 fetch(requestUrl)
 	.then(response => response.json())
 	.then(data => {
@@ -9,6 +11,7 @@ console.log (JSON.stringify(data));
 
 
 */
+/*
 const req = new XMLHttpRequest();
 req.open("GET",requestUrl, true);
 req.send();
@@ -16,3 +19,18 @@ req.onload = function(){
   const json = JSON.parse(req.responseText);
 console.log(JSON.stringify(json));
 };
+*/
+//this one get's the data in a different manner
+
+d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
+d3.autoType)
+.then (function(data){
+	console.log(data)
+	d3.select("body")
+  .selectAll("div")
+  .data(data)
+  .enter()
+  .append("div")
+  .style("width", function(d) { return d.amount * 40 + "px"; })
+  .style("height", "15px");
+});
