21f0c367704a5e7028f03ba51f9a0b1d01598234
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-01-17 00:05:53 +0300

    now all three methods get the data to the <p> element, for some reasons I found that to be important to learn

--- script.js
+++ script.js
@@ -1,59 +1,40 @@
-
+window.addEventListener('load', (event) => {
 
 let requestUrl= "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
 //Both of the following methods get the data correctly
-
+/*
 fetch(requestUrl)
 	.then(response => response.json())
 	.then(data => {
     document.getElementById('message').innerHTML = JSON.stringify(data);
 	})
+*/
 
 
 
-/*
 const req = new XMLHttpRequest();
 req.open("GET",requestUrl, true);
 req.send();
 req.onload = function(){
   const json = JSON.parse(req.responseText);
-console.log(JSON.stringify(json));
+  document.getElementsByClassName('message')[0].textContent = JSON.stringify(json);
+ const dataset= JSON.stringify(json);
+ console.log(dataset);
+//console.log(JSON.stringify(json));
 };
-*/
-//this one get's the data in a different manner
+
 
 /*
+//this one get's the data in a different manner
+//like this one is very organized
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
 d3.autoType)
 .then (function(data){
-	console.log(data)
-	d3.select("body")
-  .selectAll("div")
-  .data(data)
-  .enter()
-  .append("div")
-  .style("width", function(d) { return d.amount * 40 + "px"; })
-  .style("height", "15px");
+  console.log(data)
 });
 */
-/*
-var title = "Comments on Posts",
-  data = [{type: "Post A", amount: 4},
-         {type: "Post B", amount: 2},
-         {type: "Post C", amount: 7},
-         {type: "Post D", amount: 5},
-         {type: "Post E", amount: 6}]
-;
 
-d3.select("body")
-  .append("h3")
-  .text(title);
 
-  d3.select("body")
-  .selectAll("div")
-  .data(data)
-  .enter()
-  .append("div")
-  .style("width", function(d) { return d.amount * 40 + "px"; })
-  .style("height", "15px");*/
+
+});
